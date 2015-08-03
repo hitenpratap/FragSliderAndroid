4823cf5c0495691a3344ebf2edbfb5b78df11972
Hiten Pratap Singh <hitenpratap99@gmail.com>
2015-08-04 03:52:37 +0530

    Initial commit wih Sliding menu implemented

---
+++ src/com/techsect/clashmate/adapters/SliderMenuAdapter.java
@@ -0,0 +1,71 @@
+package com.techsect.clashmate.adapters;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.techsect.clashmate.R;
+import com.techsect.clashmate.models.SliderMenuItem;
+
+import java.util.ArrayList;
+
+/**
+ * Created by hitenpratap on 4/8/15.
+ */
+public class SliderMenuAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<SliderMenuItem> navDrawerItems;
+
+    public SliderMenuAdapter(Context context, ArrayList<SliderMenuItem> navDrawerItems){
+        this.context = context;
+        this.navDrawerItems = navDrawerItems;
+    }
+
+    @Override
+    public int getCount() {
+        return navDrawerItems.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return navDrawerItems.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater mInflater = (LayoutInflater)
+                    context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            convertView = mInflater.inflate(R.layout.drawer_list_item, null);
+        }
+
+        ImageView imgIcon = (ImageView) convertView.findViewById(R.id.icon);
+        TextView txtTitle = (TextView) convertView.findViewById(R.id.title);
+        TextView txtCount = (TextView) convertView.findViewById(R.id.counter);
+
+        imgIcon.setImageResource(navDrawerItems.get(position).getIcon());
+        txtTitle.setText(navDrawerItems.get(position).getTitle());
+
+        // displaying count
+        // check whether it set visible or not
+        if(navDrawerItems.get(position).getCounterVisibility()){
+            txtCount.setText(navDrawerItems.get(position).getCount());
+        }else{
+            // hide the counter view
+            txtCount.setVisibility(View.GONE);
+        }
+
+        return convertView;
+    }
+
+}
